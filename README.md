8aa0ca76ffdf7c96ec969194a5031e53163d61b9
Felipe Novaes Rocha <windholyghost@gmail.com>
2022-01-06 01:48:31 -0300

    feat: add cors, readme

---
+++ README.md
@@ -0,0 +1,10 @@
+### Some guide to run
+
+1. Define and configure the envs, see the `env.sample`
+2. `npm && npm start`
+
+### About unit tests
+
+`npm test`
+
+More info soon
--- src/app.js
+++ src/app.js
@@ -1,10 +1,13 @@
 import express from "express";
+import cors from "cors";
+
 const app = express();
 
 import { catchAsync } from "./utils/catchAsync.js";
 
 import * as daolist from "./routes/daolist.js";
 
+app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
